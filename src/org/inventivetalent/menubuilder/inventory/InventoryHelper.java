abbfb86ae33dff1b92c18dd0d8d47bedf1814302
Jakub Staroń <kubastaron@hotmail.com>
2019-05-02 19:31:53 +0200

    Add support for 1.14 and 1.13 inventory title type

--- src/org/inventivetalent/menubuilder/inventory/InventoryHelper.java
+++ src/org/inventivetalent/menubuilder/inventory/InventoryHelper.java
@@ -30,7 +30,12 @@ public class InventoryHelper {
 	public static void changeTitle(Inventory inv, String title) {
 		try {
 			Object minecrafInventory = AccessUtil.setAccessible(obcCraftInventory.getDeclaredField("inventory")).get(inv);
-			AccessUtil.setAccessible(obcMinecraftInventory.getDeclaredField("title")).set(minecrafInventory, nmsChatComponentText.getConstructor(String.class).newInstance(title));
+			try {
+				AccessUtil.setAccessible(obcMinecraftInventory.getDeclaredField("title")).set(minecrafInventory, nmsChatComponentText.getConstructor(String.class).newInstance(title));
+			}catch (IllegalArgumentException e)
+			{
+				AccessUtil.setAccessible(obcMinecraftInventory.getDeclaredField("title")).set(minecrafInventory, title);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
